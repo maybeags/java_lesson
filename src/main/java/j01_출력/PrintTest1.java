525a937c7182649099ab2b159d9285b8b1874587
maybeags <suren0709@gmail.com>
2024-06-23 22:02:44 +0900

    Java 수업 자료 준비 수정

---
+++ src/main/java/j01_출력/PrintTest1.java
@@ -0,0 +1,29 @@
+package j01_출력;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class PrintTest1 {
+    public static void main(String[] args) {
+        // 한 줄 출력
+
+        /*
+        여러줄 출력
+         */
+
+        /**
+         * 클래스 또는 메서드의 역할을 명시 또는 설명하기 위한 주석
+         */
+
+        // 문자열 출력
+        System.out.println("이름: ");
+        System.out.println("안근수");
+        System.out.println("주소: ");
+        System.out.println("부산광역시 연제구");
+
+        boolean isUsernameCheckFlag = true;
+    }
+
+
+
+
+}
